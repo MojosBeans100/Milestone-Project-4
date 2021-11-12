3459fc239e56bb5e120afaaae76b91b210b436a6
MojosBeans100 <lucy.colley@btinternet.com>
2021-11-12 14:30:58 +0000

    Add first rev of PipelineList models

--- spaceport/models.py
+++ spaceport/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class PipelineList(models.Model):
+
+    # status choices
+    status_choices = (
+        ('active', 'Active'),
+        ('complete', 'Complete'),
+        ('in_progress', 'In Progress'),
+    )
+
+    # auto generated
+    pipeline_id     = models.IntegerField(primary_key=True)
+    created_by      = models.CharField(max_length=50)
+    date_created    = models.DateTimeField(auto_now=True)
+    status          = models.CharField(choices=status_choices, max_length=20)
+
+    # user form
+    pipeline_name   = models.CharField(max_length=30)
+    pipeline_des    = models.TextField()
+    aoi             = models.JSONField()
+    start_date      = models.DateTimeField()
+    end_date        = models.DateTimeField()
+
+
