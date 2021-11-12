e7d7bf0c306c8f5185fadaa51b59a562e8a9ac13
MojosBeans100 <lucy.colley@btinternet.com>
2021-11-12 15:06:41 +0000

    Filter list of pipelines by active/complete status to display on models_temp html

--- spaceport/views.py
+++ spaceport/views.py
@@ -6,8 +6,10 @@ def homepage(request):
     return render(request, 'homepage.html')
 
 def models_temp(request):
+
     context = {
-        'list' : PipelineList.objects.all()
+        'active_list' : PipelineList.objects.filter(status="active"),
+        'complete_list' : PipelineList.objects.filter(status="complete")
     }
 
     print(context)
