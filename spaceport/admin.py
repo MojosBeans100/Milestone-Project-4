a65b142622d45e892606f16245ce2c37fba96949
MojosBeans100 <lucy.colley@btinternet.com>
2021-11-12 14:34:43 +0000

    Add PipelineLists model to admin panel

--- spaceport/admin.py
+++ spaceport/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import PipelineList
 
 # Register your models here.
+@admin.register(PipelineList)
+class PipelineListAdmin(admin.ModelAdmin):
+    list_display = (
+                'pipeline_id',
+                'pipeline_name',
+                'pipeline_des',
+                'created_by',
+                'date_created',
+                'status',                
+                )
