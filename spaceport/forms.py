19118605be813c90d5e6ea512eca9282d28e6f9f
MojosBeans100 <lucy.colley@btinternet.com>
2021-11-13 13:33:22 +0000

    Add widgets to form for start date, end date, pipeline description

--- spaceport/forms.py
+++ spaceport/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, DateInput, TextInput
+from django.forms import ModelForm, DateInput, TextInput, Textarea
 from .models import PipelineList
 
 class CreatePipeline(ModelForm):
@@ -15,6 +15,23 @@ class CreatePipeline(ModelForm):
         widgets = {
             'pipeline_name': TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'eg. My First Pipeline'
-            })
+                'placeholder': 'eg. My First Pipeline',
+                'id': 'pipeline_name',
+            }),
+            'pipeline_des': Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Give your pipeline a description',
+                'id': 'pipeline_des'
+            }),
+            'start_date': DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control',
+                'id': 'start_date',
+            }),
+            'end_date': DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control',
+                'id': 'end_date',
+            }),
         }
